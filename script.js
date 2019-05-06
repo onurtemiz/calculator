af30126b181d303dedf23279af4a90361f69fd01
Onur Temiz <onurmain@gmail.com>
2019-05-06 20:14:50 +0300

    Totally Working Calculator Without Keypress

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const operators = document.querySelectorAll(".operator");
 const clearButton = document.querySelector("#clear");
 const equalButton = document.querySelector("#equal");
 
-let onlyOne = ["+", "*", "/", "-", ","];
+let onlyOne = ["+", "*", "/", "-", "."];
 
 numbers.forEach(function(number) {
   number.addEventListener("mouseup", function() {
@@ -42,15 +42,22 @@ equalButton.addEventListener("mouseup", function() {
   let arr = [];
   let numArr = [];
   for (let i = 0; i < toParse.length; i++) {
-    if (!isNaN(toParse[i])) {
+    if (!isNaN(toParse[i]) || toParse[i] == ".") {
       numArr.push(toParse[i]);
+      if (i == toParse.length - 1) {
+        let numMulti = "";
+        for (let j = 0; j < numArr.length; j++) {
+          numMulti += numArr[j];
+        }
+        arr.push([numMulti]);
+      }
     } else {
       if (numArr.length > 1) {
         let numMulti = "";
         for (let j = 0; j < numArr.length; j++) {
           numMulti += numArr[j];
         }
-        arr.push(numMulti);
+        arr.push([numMulti]);
       } else {
         arr.push(numArr);
       }
@@ -58,43 +65,75 @@ equalButton.addEventListener("mouseup", function() {
       arr.push([toParse[i]]);
     }
   }
-  if (numArr.length > 0) {
-    arr.push(numArr);
-  }
 
-  let dominantOperatorIndexes = [];
-  let normalOperatorIndexes = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] == "/" || arr[i] == "*") {
-      dominantOperatorIndexes.push(arr.indexOf(arr[i]));
-    } else if (arr[i] == "+" || arr[i] == "-") {
-      normalOperatorIndexes.push(arr.indexOf(arr[i]));
-    }
-  }
+  let dominantOperatorIndexes = findOperations(arr)[0];
+  let normalOperatorIndexes = findOperations(arr)[1];
 
-  for (let i = 0; i < dominantOperatorIndexes.length; i++) {
-    let num1 = arr[dominantOperatorIndexes[i] - 1];
-    let num2 = arr[dominantOperatorIndexes[i] + 1];
-    let ope = arr[dominantOperatorIndexes[i]];
-    console.log(num1);
-    console.log(num2);
-    console.log(ope);
+  //console.log(arr);
+
+  while (dominantOperatorIndexes.length > 0) {
+    let num1 = arr[dominantOperatorIndexes[0] - 1];
+    let num2 = arr[dominantOperatorIndexes[0] + 1];
+    let ope = arr[dominantOperatorIndexes[0]];
     let opeResult = operator(num1, num2, ope);
-    console.log(opeResult);
+    let newArr = [];
+    for (let j = 0; j < dominantOperatorIndexes[0] - 1; j++) {
+      newArr.push(arr[j]);
+    }
+    newArr.push(opeResult);
+    for (let j = dominantOperatorIndexes[0] + 2; j < arr.length; j++) {
+      newArr.push(arr[j]);
+    }
+    arr = newArr;
+    dominantOperatorIndexes = findOperations(arr)[0];
+    normalOperatorIndexes = findOperations(arr)[1];
   }
+  while (normalOperatorIndexes.length > 0) {
+    let num1 = arr[normalOperatorIndexes[0] - 1];
+    let num2 = arr[normalOperatorIndexes[0] + 1];
+    let ope = arr[normalOperatorIndexes[0]];
+    let opeResult = operator(num1, num2, ope);
+    let newArr = [];
+    for (let j = 0; j < normalOperatorIndexes[0] - 1; j++) {
+      newArr.push(arr[j]);
+    }
+    newArr.push(opeResult);
+    for (let j = normalOperatorIndexes[0] + 2; j < arr.length; j++) {
+      newArr.push(arr[j]);
+    }
+    arr = newArr;
+    dominantOperatorIndexes = findOperations(arr)[0];
+    normalOperatorIndexes = findOperations(arr)[1];
+  }
+  const operations = document.querySelector("#operations");
+  operations.textContent = result.textContent;
+  result.textContent = arr[0];
 });
 
 function operator(num1, num2, ope) {
   if (ope == "+") {
-    return num1 + num2;
+    return Number(num1) + Number(num2);
   }
   if (ope == "-") {
-    return num1 - num2;
+    return Number(num1) - Number(num2);
   }
   if (ope == "*") {
-    return num1 * num2;
+    return Number(num1) * Number(num2);
   }
   if (ope == "/") {
-    return num1 / num2;
+    return Number(num1) / Number(num2);
   }
+}
+
+function findOperations(arr) {
+  let dominant = [];
+  let normal = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == "/" || arr[i] == "*") {
+      dominant.push(arr.indexOf(arr[i]));
+    } else if (arr[i] == "+" || arr[i] == "-") {
+      normal.push(arr.indexOf(arr[i]));
+    }
+  }
+  return [dominant, normal];
 }
